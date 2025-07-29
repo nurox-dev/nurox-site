2ea43c8aeb579b98c7c7d8412c7334b673d13d66
Anish <anishahsarvatraasti@gmail.com>
2025-07-29 07:34:06 +0000

    for both header  and footer only increase intensity of tint , greater th

--- src/components/landing/Header.tsx
+++ src/components/landing/Header.tsx
@@ -11,7 +11,7 @@ const NavLink = ({ href, children }: { href: string, children: React.ReactNode }
 
 export default function Header() {
   return (
-    <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 rounded-b-2xl bg-black/20">
+    <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-primary/10 backdrop-blur supports-[backdrop-filter]:bg-primary/5 rounded-b-2xl">
       <div className="container flex h-14 max-w-screen-2xl items-center">
         <div className="flex items-center md:hidden">
             <Link href="/" className="flex items-center space-x-2">
