ebc90e332a4fc0889fe8ad1fd878285422b4783f
Anish <anishahsarvatraasti@gmail.com>
2025-08-01 17:24:22 +0000

    i cant see any changes there might be issue

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -19,7 +19,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={cn('min-h-screen bg-background font-sans antialiased', inter.variable)}>
+      <main className={cn('min-h-screen font-sans antialiased', inter.variable)}>
         <Component {...pageProps} />
         <Toaster />
       </main>
