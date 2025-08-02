d2bba34f70b71a7144cb7b7a43edc10fd2602cb9
Anish <anishahsarvatraasti@gmail.com>
2025-08-02 05:56:17 +0000

    add fav icon use my site logo svg

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -17,7 +17,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           content="Nurox powers your business with AI to automate processes and boost sales. Let our technology work behind the scenes so your success can shine."
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
       </Head>
       <main className={cn('min-h-screen font-sans antialiased', inter.variable)}>
         <Component {...pageProps} />
